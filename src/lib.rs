81ae0e45d8ca6c40c206e3c0fdf7a3a3c5b1301a
HampusM <hampus@hampusmat.com>
2026-07-12 16:00:56 +0200

    replace i8 pointers with c_char pointers

--- src/lib.rs
+++ src/lib.rs
@@ -745,7 +745,7 @@ impl CompilerOptions {
 		self
 	}
 
-	fn push_strings(mut self, name: CompilerOptionName, s0: *const i8, s1: *const i8) -> Self {
+	fn push_strings(mut self, name: CompilerOptionName, s0: *const c_char, s1: *const c_char) -> Self {
 		self.options.push(sys::slang_CompilerOptionEntry {
 			name,
 			value: sys::slang_CompilerOptionValue {
